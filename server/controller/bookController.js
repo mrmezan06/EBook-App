21cace739642924f1bbe4d703b64748ca9385a4b
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2022-10-05 17:23:14 +0600

    Books Route and Controller , middleware for checking user and admin created

--- server/controller/bookController.js
+++ server/controller/bookController.js
@@ -22,3 +22,44 @@ exports.Upload = async (req, res) => {
     });
   }
 };
+
+exports.GetBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
+
+// Update a book
+exports.UpdateBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+// Delete a book
+exports.DeleteBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,7 +1,19 @@
-const { Upload } = require("../controller/bookController");
+const {
+  Upload,
+  GetBooks,
+  DeleteBook,
+  UpdateBook,
+} = require("../controller/bookController");
+const { verifyAdmin, verifyUser } = require("../utils/Verify");
 
 const router = require("express").Router();
 
-router.post("/upload/:id", Upload);
+router.post("/upload/:uid", verifyUser, Upload);
+router.get("/getBooks", GetBooks);
+router.delete("/delete/:id", verifyAdmin, DeleteBook);
+// Update by User
+router.put("/update/user/:uid/:id", verifyUser, UpdateBook);
+// Update by Admin
+router.put("/update/admin/:id", verifyAdmin, UpdateBook);
 
 module.exports = router;
