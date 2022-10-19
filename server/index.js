b49cd34e45251cf0d227e67ebcf15b7c70b48e64
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2022-10-20 05:52:53 +0600

    Adding View Counter Functionality

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const cokieParser = require("cookie-parser");
 const userRoutes = require("./routes/userRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 
+// ViewSchema
+const Viewer = require("./model/viewer");
+
 dotenv.config();
 
 const app = express();
@@ -26,3 +29,44 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.use("/auth", userRoutes);
 app.use("/books", bookRoutes);
+
+// Count Viewers
+app.post("/count", async (req, res) => {
+  const { bookId } = req.body;
+  try {
+    if (bookId) {
+      // Check if bookId exists in bookViewCount array
+      const bookViewCount = await Viewer.findOne({
+        bookViewCount: { $elemMatch: { bookId } },
+      });
+      // If it does, increment count
+      if (bookViewCount) {
+        await Viewer.updateOne(
+          { bookViewCount: { $elemMatch: { bookId } } },
+          { $inc: { "bookViewCount.$.count": 1, count: 1 } },
+          { new: true }
+        );
+      } else {
+        // If it doesn't, add bookId to bookViewCount array and set count to 1
+        await Viewer.findOneAndUpdate(
+          {},
+          {
+            $push: { bookViewCount: { bookId, count: 1 } },
+            $inc: { count: 1 },
+          },
+          { new: true }
+        );
+      }
+    } else {
+      await Viewer.findOneAndUpdate(
+        {},
+        { $inc: { count: 1 } },
+        { new: true, upsert: true }
+      );
+    }
+    const viewer = await Viewer.findOne({});
+    res.status(200).json(viewer);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
